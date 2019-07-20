c1b6b9ade2d334e121c9c410d1924b425dbdacdb
Josef Richter <josef.richter@me.com>
2019-07-20 17:20:01 +0200

    minor cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
@@ -37,40 +37,6 @@ const IndexPage = () => (
       bringing solid results. Please feel free to leave your comments,
       suggestions or questions.
     </p>
-    <ul class="pageslist">
-      <li>
-        <Link to="/1-categorize-devices/">1. Categorize Devices</Link>
-      </li>
-      <li>
-        <Link to="/2-split-to-triggers-vs-actors/">
-          2. Split to Triggers vs Actors
-        </Link>
-      </li>
-      <li>
-        <Link to="/3-generate-all-combinations/">
-          3. Generate all combinations
-        </Link>
-      </li>
-      <li>
-        <Link to="/4-rate-all-combinations/">4. Rate all combinations</Link>
-      </li>
-      <li>
-        <Link to="/5-cluster-useful-combinations/">
-          5. Cluster useful combinations
-        </Link>
-      </li>
-      <li>
-        <Link to="/6-scenes-for-given-packages/">
-          6. Scenes for given packages
-        </Link>
-      </li>
-      <li>
-        <Link to="/7-design-templates/">7. Design templates</Link>
-      </li>
-      <li>
-        <Link to="/8-functional-prototype/">8. Functional prototype</Link>
-      </li>
-    </ul>
   </Layout>
 )
 
